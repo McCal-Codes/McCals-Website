ddca829414509c19b8d4383d0dfd3a2557f77a0a
McCal-Codes <McCal-Codes@users.noreply.github.com>
2026-04-06 12:18:10 -0400

    feat(site): update App routing and components

--- sites/mcc-cal-vite/src/components/about/BioSection.tsx
+++ sites/mcc-cal-vite/src/components/about/BioSection.tsx
@@ -132,10 +132,8 @@ export function BioSection({ className = '' }: BioSectionProps) {
                   </svg>
                   Contact page
                 </Link>
-                <a
-                  href="https://calendly.com/cjmccar-mcc-cal/30min"
-                  target="_blank"
-                  rel="noopener noreferrer"
+                <Link
+                  to="/grab-coffee"
                   className={styles.menuItem}
                   role="menuitem"
                 >
@@ -143,7 +141,7 @@ export function BioSection({ className = '' }: BioSectionProps) {
                     <path d="M19 3h-1V1h-2v2H8V1H6v2H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11zM9 10H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm-8 4H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2z" />
                   </svg>
                   Grab a coffee
-                </a>
+                </Link>
               </div>
             </div>
 
