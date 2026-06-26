c5cbf5d752347e6267866d03618d557b650b0bfe
McCal-Codes <175259256+McCal-Codes@users.noreply.github.com>
2026-06-26 14:15:57 -0400

    fix: downgrade lighthouse version to 12.6.1 for compatibility
    feat: add Vercel Analytics component to Layout for enhanced tracking
    
    fix: update tsconfig to enable noImplicitAny for stricter type checking

--- sites/mcc-cal-vite/src/components/Layout/Layout.tsx
+++ sites/mcc-cal-vite/src/components/Layout/Layout.tsx
@@ -1,16 +1,23 @@
+// src/components/layout/Layout_COPY.tsx
+
 import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
 import ThemeToggle from '../ThemeToggle';
+import { Analytics } from '@vercel/analytics/react';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <div className="site-layout site-layout--standard">
     <Nav />
+
     <main className="site-main">
       <div className="site-container">{children}</div>
     </main>
+
     <Footer />
     <ThemeToggle />
+
+    <Analytics />
   </div>
 );
 
